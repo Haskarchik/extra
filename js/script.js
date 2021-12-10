ccba8df23bda20a185769a3665c60e21b47e5904
Volodya <55237974+Haskarchik@users.noreply.github.com>
2021-12-10 20:10:14 +0200

    Pre-final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const swiper = new Swiper('.swiper', {
 
 	loop: true,
@@ -6,11 +7,109 @@ const swiper = new Swiper('.swiper', {
 	  prevEl: '.swiper-button-prev',
 	},
 	autoplay: {
-		delay: 3500,
+		delay: 10000,
 		stopOnLastSlide: false,
 		disableOnInteraction: false
 	
 	},
+
+	simulateTouch: false,
+	centeredSlides: true,
 	speed: 1200,
 
+		// События
+		on: {
+			// Событие инициализации
+			init: function () {
+				
+			},
+			// Событие смены слайда
+			slideChange: function () {
+
+				
+			
+				
+			}
+		},
+/*
+		// Брейк поинты (адаптив)
+	// Ширина экрана
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		480: {
+			slidesPerView: 2,
+		},
+		992: {
+			slidesPerView: 3,
+		}
+	},
+	
+	
+	// Брейк поинты (адаптив)
+	// Соотношение сторон
+	breakpoints: {
+		'@0.75': {
+			slidesPerView: 1,
+		},
+		'@1.00': {
+			slidesPerView: 2,
+		},
+		'@1.50': {
+			slidesPerView: 3,
+		}
+	},
+	*/
+
   });
+
+
+
+
+  const animItems = document.querySelectorAll('.jurnal-card');
+  
+  if (animItems.length > 0) {
+	  window.addEventListener('scroll', animOnScroll);
+	  function animOnScroll() {
+		  for (let index = 0; index < animItems.length; index++) {
+			  const animItem = animItems[index];
+			  const animItemHeight = animItem.offsetHeight;
+			  const animItemOffset = offset(animItem).top;
+			  const animStart = 4;
+  
+			  let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			  if (animItemHeight > window.innerHeight) {
+				  animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			  }
+  
+			  if ((window.scrollY > animItemOffset - animItemPoint) && window.scrollY < (animItemOffset + animItemHeight)) {
+				  animItem.classList.add('_active');
+			  } else {
+				  if (!animItem.classList.contains('_anim-no-hide')) {
+					  animItem.classList.remove('_active');
+				  }
+			  }
+		  }
+	  }
+	  function offset(el) {
+		  const rect = el.getBoundingClientRect(),
+			  scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+			  scrollTop = window.scrollY || document.documentElement.scrollTop;
+		  return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	  }
+  
+	  
+  }
+  // Меню бургер
+ 
+const headerBurger = document.querySelector('.header-burger');
+const navBody = document.querySelector('.header-nav');
+if (headerBurger) {
+	headerBurger.addEventListener("click", function () {
+		headerBurger.classList.toggle('_active');
+		navBody.classList.toggle('_active');
+	});
+}
+
+
