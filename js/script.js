e36dd56a4ec0c5c307b8eba00012cef2493fb986
Volodya <55237974+Haskarchik@users.noreply.github.com>
2022-01-16 16:10:55 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    //	autoplay: {
-    //		delay: 10000,
-    //		stopOnLastSlide: false,
-    //		disableOnInteraction: false
-    //
-    //	},
+    	autoplay: {
+    		delay: 10000,
+    		stopOnLastSlide: false,
+    		disableOnInteraction: false
+    
+    	},
 
 
     centeredSlides: true,
@@ -149,6 +149,7 @@ document.onclick = event => {
                 elem.classList.add('hide');
             }
         });
+        categoryList.classList.add('_hide');
     }else if (event.target.classList.contains('category-mobile')) {
         
         
